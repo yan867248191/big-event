676a91ba352c4a5b2c37f03933759651eb0e7d6d
YZR <867248191@qq.com>
2020-07-27 18:40:01 +0800

    登录页面注册搞好

---
+++ assets/js/login.js
@@ -0,0 +1,70 @@
+$(function(){
+    $('#link_reg').on('click',function(){
+        $('.login-box').hide();
+        $('.reg-box').show();
+    })
+    $('#link_login').on('click',function(){
+        $('.login-box').show();
+        $('.reg-box').hide();
+    })
+
+    // 自定义校验规则
+    var form = layui.form
+    var layer = layui.layer
+
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+        ] ,
+        repwd:function(value){
+            var pwd = $('.reg-box [name = password]').val()
+            if(pwd != value){
+                return '两次密码不一致'
+            }
+        }
+    })
+
+    // 监测注册功能
+    $('#form_reg').on('submit',function(e){
+        e.preventDefault();
+
+
+        var data = {
+            username:$('#form_reg [name = username]').val(),
+            password:$('#form_reg [name = password]').val()
+        }
+        $.post('http://ajax.frontend.itheima.net/api/reguser',data,function(res){
+            if(res.status != 0){
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！');
+
+            $('#link_login').click();
+        })
+    })
+
+
+    // // 监听登录提交功能
+    // $('#form_login').submit(function(e){
+    //     e.preventDefault()
+
+    //     $.ajax({
+    //         url:'http://ajax.frontend.itheima.net/api/login',
+    //         method:'POST',
+    //         data:$(this).serialize(),
+    //         success:function(res){
+    //             if(res.status != 0){
+    //                 return layer.msg('登录失败！')
+    //             }
+    //             layer.msg('登录成功！')
+
+    //             // 将登录成功得到的token字符串，保存到localstorage中
+    //             localStorage.setItem('token',res.token)
+
+    //             localStorage.href = '/index.html'
+    //         }
+    //     })
+    // })
+
+})
